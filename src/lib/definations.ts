a6ac8da719f307e61442ce23143d0816b091dcd0
AdilAhmed976 <adilahmedah976@gmail.com>
2025-05-12 19:01:07 +0530

    fix : remove name key from signupFormSchema

--- src/lib/definations.ts
+++ src/lib/definations.ts
@@ -19,10 +19,6 @@ export const loginFormSchema = z.object({
 
 export const signupFormSchema = z
   .object({
-    name: z
-      .string()
-      .min(2, { message: "Name must be at least 2 characters" })
-      .optional(),
     email: z.string().email({ message: "Invalid email address" }),
     password: z
       .string()
