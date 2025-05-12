9f3cf3aad8c8429cb0cb36e92887a16b2627f5cf
AdilAhmed976 <adilahmedah976@gmail.com>
2025-05-13 02:53:45 +0530

    fix : removed the hardcoded url & used getApiUrl() function to get the base URL and added the error of the env variable is not available

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
+import { getApiUrl } from "@/lib/get-urls";
+
 export const login = async (email: string, password: string) => {
-  const response = await fetch("http://localhost:3000/api/users/login", {
+  const response = await fetch(`${getApiUrl()}/users/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -10,7 +12,7 @@ export const login = async (email: string, password: string) => {
 };
 
 export const register = async (email: string, password: string) => {
-  const response = await fetch("http://localhost:3000/api/users/register", {
+  const response = await fetch(`${getApiUrl()}/users/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
