49492e66fab00c171c2a7afe210ab8b78abfc27e
AdilAhmed976 <adilahmedah976@gmail.com>
2025-05-13 02:28:51 +0530

    fix : Token expired in 7 daya in milliseconds fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const COOKIE_KEY = "get-set-pin-token";
-export const TOKEN_EXPIRED_AT = 7 * 24 * 60 * 60 * 1000; // 7 day in milliseconds
+export const TOKEN_EXPIRED_AT = (7 * 24 * 60 * 60 * 1000) / 1000; // 7 day in milliseconds
