83ff3da7e91a05cdcad26548f5eccdfe6c52aa9c
AdilAhmed976 <adilahmedah976@gmail.com>
2025-05-13 02:30:10 +0530

    fix: use variable reference instead of hardcoded string

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,24 +1,23 @@
+import { COOKIE_KEY, TOKEN_EXPIRED_AT } from "@/constants";
 import { deleteCookie, setCookie, useGetCookie } from "cookies-next/client";
-
-export const TOKEN_EXPIRED_AT = 7 * 24 * 60 * 60 * 1000; // 1 day in millisecond
 
 export async function createSession(token: string) {
   // Set the cookie using cookies-next (on the client side)
   if (token) {
     console.log("Setting token cookie...");
-    setCookie("token", token, {
-      maxAge: TOKEN_EXPIRED_AT / 1000,
+    setCookie(COOKIE_KEY, token, {
+      maxAge: TOKEN_EXPIRED_AT,
       path: "/",
     });
   }
 }
 
 export async function deleteSession() {
-  deleteCookie("token");
+  deleteCookie(COOKIE_KEY);
 }
 
 // used to check if the user is logged in or not
 export async function isLoggedInSession() {
   const getCookie = useGetCookie();
-  return getCookie("token") ? true : false;
+  return getCookie(COOKIE_KEY) ? true : false;
 }
