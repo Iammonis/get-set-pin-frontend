2e5b1e06905b16f94d125d3a2016949e71c89f1a
AdilAhmed976 <adilahmedah976@gmail.com>
2025-05-14 15:17:53 +0530

    feat(theme): add next theme support for dark and light modes

--- src/components/layouts/dashboard/dashboard-header.tsx
+++ src/components/layouts/dashboard/dashboard-header.tsx
@@ -3,10 +3,13 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "../../ui/button";
 import { Bell } from "lucide-react";
+import { ThemeSwitcher } from "@/components/ui/theme-switcher";
+import { useTheme } from "next-themes";
 
 type Props = {};
 
 const DashboardHeader = (props: Props) => {
+  const { setTheme } = useTheme();
   return (
     <header className="sticky top-0 z-50 border-b bg-sidebar h-14">
       <nav className="border-gray-200 px-4 lg:px-1 xl:px-4 py-2 h-full flex items-center">
@@ -18,7 +21,8 @@ const DashboardHeader = (props: Props) => {
             />
             <Separator orientation="vertical" className="h-6" />
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center gap-2">
+            <ThemeSwitcher defaultValue="system" onChange={setTheme} />
             <Button variant="secondary">
               <Bell />
             </Button>
